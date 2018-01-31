bf358d0c7e083efe834faf5d24530b4c0e9243a7
Mario <mjvinti@gmail.com>
2018-01-30 21:56:10 -0500

    Fixed misspelling mongoose.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
-
 var app = express();
 const port = process.env.PORT || 3000;
 
